e765e3d5cdf7b482b34b9f8a3df9aee4cabbd700
Alexander Hawkins <ahawkins@drdoom.highpoint.edu>
2017-01-30 12:30:56 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Alexander Hawkins on 1/30/17.
+//  Copyright © 2017 Alexander Hawkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
